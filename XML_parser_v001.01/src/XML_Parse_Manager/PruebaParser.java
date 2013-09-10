25505337b475cccf8d19cf283eda05f77eb350e7
Benelli Javier <javimg84@hotmail.com>
2013-09-10 12:14:06 +0200

    Prueba04 clase XML.java creada

--- XML_parser_v001.01/src/XML_Parse_Manager/PruebaParser.java
+++ XML_parser_v001.01/src/XML_Parse_Manager/PruebaParser.java
@@ -8,7 +8,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class PruebaParser {
+public class PruebaParser{
 
     public static void main(String args[]) {
         try {
