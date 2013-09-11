c5776290c0bc470941df13fdb4ab23c9c932221b
Adesthor <adriantorralba@hotmail.com>
2013-09-11 15:44:36 +0200

    Prueba commit

--- XML_parser_v001.01/src/Front.java
+++ XML_parser_v001.01/src/Front.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JFrame;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -25,6 +28,8 @@ public class Front extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        VCargaXML = new javax.swing.JFrame();
+        jFileChooser1 = new javax.swing.JFileChooser();
         TxtCUPS = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         BtSearch = new javax.swing.JButton();
@@ -33,6 +38,24 @@ public class Front extends javax.swing.JPanel {
         jLabel3 = new javax.swing.JLabel();
         LProceso = new javax.swing.JLabel();
         LPaso = new javax.swing.JLabel();
+        BtCargaXML = new javax.swing.JButton();
+
+        jFileChooser1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jFileChooser1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout VCargaXMLLayout = new javax.swing.GroupLayout(VCargaXML.getContentPane());
+        VCargaXML.getContentPane().setLayout(VCargaXMLLayout);
+        VCargaXMLLayout.setHorizontalGroup(
+            VCargaXMLLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jFileChooser1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        VCargaXMLLayout.setVerticalGroup(
+            VCargaXMLLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jFileChooser1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel1.setText("CUPS");
@@ -47,17 +70,28 @@ public class Front extends javax.swing.JPanel {
 
         LPaso.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
 
+        BtCargaXML.setText("Cargar XML");
+        BtCargaXML.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtCargaXMLActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.TRAILING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(29, 29, 29)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(TxtCUPS, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(BtSearch)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(BtCargaXML)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(TxtCUPS, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(BtSearch)))
                 .addGap(61, 61, 61)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -67,7 +101,6 @@ public class Front extends javax.swing.JPanel {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(LPaso, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(88, 88, 88))
-            .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.TRAILING)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -81,16 +114,31 @@ public class Front extends javax.swing.JPanel {
                     .addComponent(jLabel3)
                     .addComponent(LProceso)
                     .addComponent(LPaso))
-                .addGap(18, 18, 18)
+                .addGap(12, 12, 12)
+                .addComponent(BtCargaXML)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(462, Short.MAX_VALUE))
+                .addContainerGap(434, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BtCargaXMLActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtCargaXMLActionPerformed
+
+       jFileChooser1.showOpenDialog(this);
+    }//GEN-LAST:event_BtCargaXMLActionPerformed
+
+    private void jFileChooser1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jFileChooser1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jFileChooser1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BtCargaXML;
     private javax.swing.JButton BtSearch;
     private javax.swing.JLabel LPaso;
     private javax.swing.JLabel LProceso;
     private javax.swing.JTextField TxtCUPS;
+    private javax.swing.JFrame VCargaXML;
+    private javax.swing.JFileChooser jFileChooser1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
